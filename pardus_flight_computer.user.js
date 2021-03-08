f339dd49a67c965a597c58c8ea9fa1f62c8c398e
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2021-03-08 20:15:18 +0000

    Adding coords -> mapper urls

--- pardus_flight_computer.user.js
+++ pardus_flight_computer.user.js
@@ -8,6 +8,8 @@
 // @grant           GM_setValue
 // @grant           GM_getValue
 // @require         https://raw.githubusercontent.com/Tro95/Pardus-Options-Library/v2.4/pardus_options_library.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/raw/main/data/sectors.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/raw/main/utility/helpers.js
 // @downloadURL     https://github.com/Tro95/Pardus-Flight-Computer/raw/main/pardus_flight_computer.user.js
 // @updateURL       https://github.com/Tro95/Pardus-Flight-Computer/raw/main/pardus_flight_computer.meta.js
 // ==/UserScript==
---
+++ utility/helpers.js
@@ -0,0 +1,26 @@
+function mapper_urls_from_route(route = []) {
+    const sector_routes = {};
+
+    for (const index in route) {
+        const tile_obj = get_sector_coords_obj(route[index]);
+
+        if (!sector_routes[tile_obj.sector]) {
+            sector_routes[tile_obj.sector] = [];
+        }
+
+        sector_routes[tile_obj.sector].push(`${tile_obj.x},${tile_obj.y}`);
+    }
+
+
+    const mapper_urls = [];
+
+    for (const sector_route_index in sector_routes) {
+        mapper_urls.push(mapper_url_from_values(sector_route_index, sector_routes[sector_route_index]));
+    }
+
+    return mapper_urls;
+}
+
+function mapper_url_from_values(sector, coords) {
+    return `http://map.xcom-alliance.info/${encodeURI(sector)}.html?hilite=${coords.join('|')}`;
+}
