7737e26ce42fd70bc029c757ab2b9ec95e79df3c
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2021-03-13 22:49:49 +0000

    Tagging versions

--- pardus_flight_computer.meta.js
+++ pardus_flight_computer.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            Pardus Flight Computer
-// @version         0.3.3
+// @version         0.3.4
 // @description     Flight Computer to assist pathfinding in Pardus
 // @author          Tro (Artemis)
 // @include         http*://*.pardus.at/main.php
@@ -8,11 +8,11 @@
 // @grant           GM_setValue
 // @grant           GM_getValue
 // @require         https://raw.githubusercontent.com/Tro95/Pardus-Options-Library/v2.4/pardus_options_library.js
-// @require         https://github.com/Tro95/Pardus-Flight-Computer/raw/main/data/colours.js
-// @require         https://github.com/Tro95/Pardus-Flight-Computer/raw/main/data/sectors.js
-// @require         https://github.com/Tro95/Pardus-Flight-Computer/raw/main/pages/options.js
-// @require         https://github.com/Tro95/Pardus-Flight-Computer/raw/main/pages/main.js
-// @require         https://github.com/Tro95/Pardus-Flight-Computer/raw/main/utility/helpers.js
-// @downloadURL     https://github.com/Tro95/Pardus-Flight-Computer/raw/main/pardus_flight_computer.user.js
-// @updateURL       https://github.com/Tro95/Pardus-Flight-Computer/raw/main/pardus_flight_computer.meta.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.3.4/data/colours.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.3.4/data/sectors.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.3.4/pages/options.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.3.4/pages/main.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.3.4/utility/helpers.js
+// @downloadURL     https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.3.4/pardus_flight_computer.user.js
+// @updateURL       https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.3.4/pardus_flight_computer.meta.js
 // ==/UserScript==
