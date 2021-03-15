a6a22aeb9c192045c607bbf4506763e3283617a0
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2021-03-15 15:46:19 +0000

    v0.4.1

--- pardus_flight_computer.user.js
+++ pardus_flight_computer.user.js
@@ -8,13 +8,13 @@
 // @grant           GM_setValue
 // @grant           GM_getValue
 // @require         https://raw.githubusercontent.com/Tro95/Pardus-Options-Library/v2.4/pardus_options_library.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.4/data/colours.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.4/data/sectors.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.4/pages/options.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.4/pages/main.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.4/utility/helpers.js
-// @downloadURL     https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.4/pardus_flight_computer.user.js
-// @updateURL       https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.4/pardus_flight_computer.meta.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.4.1/data/colours.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.4.1/data/sectors.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.4.1/pages/options.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.4.1/pages/main.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.4.1/utility/helpers.js
+// @downloadURL     https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.4.1/pardus_flight_computer.user.js
+// @updateURL       https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.4.1/pardus_flight_computer.meta.js
 // ==/UserScript==
 
 switch (document.location.pathname) {
