799d7845818e3a004966e8b5d4fc8369b67f3d94
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2021-04-05 15:56:37 +0100

    v0.6.0

--- pardus_flight_computer.user.js
+++ pardus_flight_computer.user.js
@@ -1,23 +1,25 @@
 // ==UserScript==
 // @name            Pardus Flight Computer
-// @version         0.5.7
+// @version         0.6.0
 // @description     Flight Computer to assist pathfinding in Pardus
 // @author          Tro (Artemis)
 // @include         http*://*.pardus.at/main.php
+// @include         http*://*.pardus.at/ship2opponent_combat.php*
 // @include         http*://*.pardus.at/options.php
 // @grant           GM_setValue
 // @grant           GM_getValue
 // @require         https://raw.githubusercontent.com/Tro95/Pardus-Options-Library/v2.5.2/pardus_options_library.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.5.7/data/colours.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.5.7/data/sectors.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.5.7/pages/options.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.5.7/pages/main.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.5.7/utility/helpers.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.6.0/data/colours.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.6.0/data/sectors.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.6.0/pages/options.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.6.0/pages/main.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.6.0/pages/ship2opponent_combat.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.6.0/utility/helpers.js
 // @downloadURL     https://github.com/Tro95/Pardus-Flight-Computer/raw/main/pardus_flight_computer.user.js
 // @updateURL       https://github.com/Tro95/Pardus-Flight-Computer/raw/main/pardus_flight_computer.meta.js
 // ==/UserScript==
 
-/* global OptionsPage, MainPage */
+/* global OptionsPage, MainPage, Ship2opponent_combatPage */
 
 switch (document.location.pathname) {
     case '/options.php':
@@ -25,5 +27,8 @@ switch (document.location.pathname) {
         break;
     case '/main.php':
         new MainPage();
+        break;
+    case '/ship2opponent_combat.php':
+        new Ship2opponent_combatPage();
         break;
 }
