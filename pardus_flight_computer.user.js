7d8d0a4a6126189e99f0ee2ecf3bd0a727b494b7
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2023-05-22 14:00:11 +0100

    v0.12.15

--- pardus_flight_computer.user.js
+++ pardus_flight_computer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            Pardus Flight Computer
-// @version         0.12.14
+// @version         0.12.15
 // @description     Flight Computer to assist pathfinding in Pardus
 // @author          Tro (Artemis)
 // @icon            https://static.pardus.at/img/std/equipment/drive_interphased_enhanced.png
@@ -12,13 +12,13 @@
 // @grant           GM_setValue
 // @grant           GM_getValue
 // @require         https://raw.githubusercontent.com/Tro95/Pardus-Options-Library/v2.6.8/dist/pardus-options-library.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.14/data/colours.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.14/data/sectors.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.14/pages/options.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.14/pages/main.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.14/pages/msgframe.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.14/pages/ship2opponent_combat.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.14/utility/helpers.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.15/data/colours.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.15/data/sectors.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.15/pages/options.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.15/pages/main.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.15/pages/msgframe.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.15/pages/ship2opponent_combat.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.15/utility/helpers.js
 // @downloadURL     https://github.com/Tro95/Pardus-Flight-Computer/raw/autopilot/pardus_flight_computer.user.js
 // @updateURL       https://github.com/Tro95/Pardus-Flight-Computer/raw/autopilot/pardus_flight_computer.meta.js
 // ==/UserScript==
