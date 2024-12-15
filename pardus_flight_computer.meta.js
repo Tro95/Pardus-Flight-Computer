070fd77a56ec2cf8e9a14a6077b44518f489d67c
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2024-12-15 20:12:09 +0000

    Versioning

--- pardus_flight_computer.meta.js
+++ pardus_flight_computer.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            Pardus Flight Computer
-// @version         0.12.24
+// @version         1.0.0
 // @description     Flight Computer to assist pathfinding in Pardus
 // @author          Tro (Artemis)
 // @icon            https://static.pardus.at/img/std/equipment/drive_interphased_enhanced.png
@@ -11,14 +11,7 @@
 // @match           http*://*.pardus.at/options.php
 // @grant           GM_setValue
 // @grant           GM_getValue
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Options-Library/v2.6.8/dist/pardus-options-library.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.24/data/colours.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.24/data/sectors.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.24/pages/options.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.24/pages/main.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.24/pages/msgframe.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.24/pages/ship2opponent_combat.js
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.24/utility/helpers.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v1.0.0/dist/pardus_flight_computer.js
 // @downloadURL     https://github.com/Tro95/Pardus-Flight-Computer/raw/main/pardus_flight_computer.user.js
 // @updateURL       https://github.com/Tro95/Pardus-Flight-Computer/raw/main/pardus_flight_computer.meta.js
 // ==/UserScript==
--- pardus_flight_computer.user.js
+++ pardus_flight_computer.user.js
@@ -11,7 +11,7 @@
 // @match           http*://*.pardus.at/options.php
 // @grant           GM_setValue
 // @grant           GM_getValue
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v0.12.21/dist/pardus_flight_computer.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v1.0.0/dist/pardus_flight_computer.js
 // @downloadURL     https://github.com/Tro95/Pardus-Flight-Computer/raw/main/pardus_flight_computer.user.js
 // @updateURL       https://github.com/Tro95/Pardus-Flight-Computer/raw/main/pardus_flight_computer.meta.js
 // ==/UserScript==
