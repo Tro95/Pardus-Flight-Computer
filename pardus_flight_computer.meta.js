0e0d70264bdd0fb16e901ff93d6c6ff60e87ed3e
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2024-12-16 15:25:00 +0000

    Fixing includes URL

--- pardus_flight_computer.meta.js
+++ pardus_flight_computer.meta.js
@@ -11,7 +11,7 @@
 // @match           http*://*.pardus.at/options.php
 // @grant           GM_setValue
 // @grant           GM_getValue
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/refs/heads/new/dist/pardus_flight_computer.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/refs/heads/new/dist/pardus-flight-computer.js
 // @downloadURL     https://github.com/Tro95/Pardus-Flight-Computer/raw/refs/heads/new/pardus_flight_computer.user.js
 // @updateURL       https://github.com/Tro95/Pardus-Flight-Computer/raw/refs/heads/new/pardus_flight_computer.meta.js
 // ==/UserScript==
