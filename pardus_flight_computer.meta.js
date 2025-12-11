3332f419af2ecaadbd10f220f321a44a91b8151e
Thomas O'Brien <thomas.r.obrien@btinternet.com>
2025-12-11 23:14:32 +0000

    Preparing for v1.1.0

--- pardus_flight_computer.meta.js
+++ pardus_flight_computer.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            Pardus Flight Computer
-// @version         1.0.3
+// @version         1.1.0
 // @description     Flight Computer to assist pathfinding in Pardus
 // @author          Tro (Artemis)
 // @icon            https://static.pardus.at/img/std/equipment/drive_interphased_enhanced.png
@@ -11,7 +11,7 @@
 // @match           http*://*.pardus.at/options.php
 // @grant           GM_setValue
 // @grant           GM_getValue
-// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/refs/heads/new/dist/pardus-flight-computer.js
-// @downloadURL     https://github.com/Tro95/Pardus-Flight-Computer/raw/refs/heads/new/pardus_flight_computer.user.js
-// @updateURL       https://github.com/Tro95/Pardus-Flight-Computer/raw/refs/heads/new/pardus_flight_computer.meta.js
+// @require         https://raw.githubusercontent.com/Tro95/Pardus-Flight-Computer/v1.1.0/dist/pardus-flight-computer.js
+// @downloadURL     https://github.com/Tro95/Pardus-Flight-Computer/raw/main/pardus_flight_computer.user.js
+// @updateURL       https://github.com/Tro95/Pardus-Flight-Computer/raw/main/pardus_flight_computer.meta.js
 // ==/UserScript==
